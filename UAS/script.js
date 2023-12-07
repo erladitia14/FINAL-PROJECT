ea4d856447b91aec1b49d259edcd181596415fba
Erlangga Aditia <142164074+erladitia14@users.noreply.github.com>
2023-12-07 10:41:04 +0700

    Update script.js

--- UAS/script.js
+++ UAS/script.js
@@ -35,44 +35,104 @@
   }, 1000);
   
   const textArray = ["52%", "107 Jt"];
-  let index = 0;
-  let currentText = "";
-  let letterIndex = 0;
-  
-  function type() {
-    if (letterIndex === textArray[index].length) {
-      setTimeout(() => {
-        erase();
-      }, 2000);
-      return;
-    }
-  
-    currentText = textArray[index].substring(0, letterIndex + 1);
-    letterIndex++;
-    document.querySelector("h1.typing").innerHTML = currentText;
-    setTimeout(type, 200);
+let index = 0;
+let currentText = "";
+let letterIndex = 0;
+
+function type() {
+  if (letterIndex === textArray[index].length) {
+    setTimeout(() => {
+      erase();
+    }, 2000);
+    return;
   }
-  
-  function erase() {
-    if (letterIndex === 0) {
-      index++;
-      if (index === textArray.length) {
-        index = 0;
-      }
-      setTimeout(() => {
-        type();
-      }, 1000);
-      return;
+
+  currentText = textArray[index].substring(0, letterIndex + 1);
+  letterIndex++;
+  document.querySelector("h1.typing").innerHTML = currentText + "<span class='blink'>|</span>";
+  setTimeout(type, 200);
+}
+
+function erase() {
+  if (letterIndex === 0) {
+    index++;
+    if (index === textArray.length) {
+      index = 0;
     }
-  
-    currentText = textArray[index].substring(0, letterIndex - 1);
-    letterIndex--;
-    document.querySelector("h1.typing").innerHTML = currentText;
-    setTimeout(erase, 100);
-  }
-  
-  document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
       type();
     }, 1000);
-  });
+    return;
+  }
+
+  currentText = textArray[index].substring(0, letterIndex - 1);
+  letterIndex--;
+  document.querySelector("h1.typing").innerHTML = currentText + "<span class='blink'>|</span>";
+  setTimeout(erase, 100);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    type();
+  }, 1000);
+});
+
+$(function () {
+
+	'use strict';
+    
+    
+    
+	//scrollTrig
+	gsap.registerPlugin(ScrollTrigger);
+    
+	function scrollTrig() {
+    
+	    let gsapBl = $('.gsap__bl').width();
+	    // $('.gsap__item').css('width', gsapBl + 'px');
+	    let gsapTrack = $('.gsap__track').width();
+	    let scrollSliderTransform = gsapTrack - gsapBl;
+    
+	    let winHeight = $(window).height();
+	    let slHeight = $('.gsap_slider').outerHeight(true);
+	    let startScrollTrig = (winHeight - slHeight) / 2;
+    
+	    // Skew
+	    let proxy = { skew: 0 },
+		skewSetter = gsap.quickSetter(".gsap__item", "skewX", "deg"),
+		clamp = gsap.utils.clamp(-1000, 1000);
+    
+	    gsap.to(".gsap__track", {
+		scrollTrigger: {
+		    trigger: ".gsap_slider",
+		    // start: "top center",
+		    start: () => "-=" + startScrollTrig,
+		    end: "+=1500px",
+		    // end: () => '+=' + gsapTrack,
+		    scrub: true,
+		    pin: true,
+		    // markers: true
+		    onUpdate: (self) => {
+			let skew = clamp(self.getVelocity() / 800);
+			if (Math.abs(skew) > Math.abs(proxy.skew)) {
+			    proxy.skew = skew;
+			    gsap.to(proxy, {
+				skew: 0,
+				duration: 0.8,
+				ease: "power3",
+				overwrite: true,
+				onUpdate: () => skewSetter(proxy.skew)
+			    });
+			}
+		    }
+		},
+		x: "-" + scrollSliderTransform + "px",
+	    });
+	    gsap.set(".gsap__item", { transformOrigin: "center center", force3D: true });
+    
+	    setTimeout(() => {
+		ScrollTrigger.refresh();
+	    }, 1000);
+	}
+	scrollTrig();
+    });
